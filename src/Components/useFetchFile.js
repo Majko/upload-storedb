f6bbd299402043d6d2fbd148ed4b80de6d5f8c12
Majko <mrnhrnk@gmail.com>
2021-08-03 22:55:00 +0200

    upratanie

--- src/Components/useFetchFile.js
+++ src/Components/useFetchFile.js
@@ -1,22 +1,22 @@
 import { Storage } from "aws-amplify";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const useFetchFile = (identityID, key) => {
   const [signedUrl, setSignedUrl] = useState(null);
   const [error, setError] = useState(null);
 
   Storage.configure({ level: "protected" });
-    const fetchFile = async () => {
-      try {
-        const signedUrl = await Storage.get(key, {
-          identityId: identityID,
-        });
-        setSignedUrl(signedUrl);
-      } catch (error) {
-        setError(error);
-        console.error(error);
-      }
-    };
-    fetchFile();
+  const fetchFile = async () => {
+    try {
+      const signedUrl = await Storage.get(key, {
+        identityId: identityID,
+      });
+      setSignedUrl(signedUrl);
+    } catch (error) {
+      setError(error);
+      console.error(error);
+    }
+  };
+  fetchFile();
   return { signedUrl, error };
 };
