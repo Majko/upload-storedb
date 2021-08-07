2845a9dc0c9012627f3ae4cdd66c4bc190369856
Majko <mrnhrnk@gmail.com>
2021-08-07 16:18:33 +0200

    Cistenie a coment

--- src/Components/useFetchFile.js
+++ src/Components/useFetchFile.js
@@ -1,10 +1,17 @@
 import { Storage } from "aws-amplify";
 import { useState } from "react";
 
+/**
+ *
+ * @param {String} identityID AWS Identity ID, collected from token
+ * @param {String} key File name (key)
+ * @returns {Url} Signed Url for  the file returned
+ */
 export const useFetchFile = (identityID, key) => {
   const [signedUrl, setSignedUrl] = useState(null);
   const [error, setError] = useState(null);
 
+  //"protected" conf means that only owner can write, but everybody (who has Url) can read
   Storage.configure({ level: "protected" });
   const fetchFile = async () => {
     try {
