322f3b47efb419e759e486535346e22dbbd96de9
Majko <mrnhrnk@gmail.com>
2021-08-07 16:13:05 +0200

    Zvacsenie obrazku

--- src/Components/ShowLocalImage.js
+++ src/Components/ShowLocalImage.js
@@ -1,15 +1,27 @@
-function ShowLocalImage(props) {
-  const file = props.file
-  console.log(file);
-  return (
-    <div className="image">
-      <img
-        src={URL.createObjectURL(file)}
-        alt="inb"
-        width="500"
-        height="600"
-      />
-    </div>
-  );
+/**
+ * @description Component to show under html img tag file provided as paramater
+ * @param {file} param0 image file to be be shownn
+ * @returns none
+ */
+function ShowLocalImage({ file }) {
+  try {
+    return (
+      <div className="image">
+        <img
+          src={URL.createObjectURL(file)}
+          alt="inb"
+          width="700"
+          height="900"
+        />
+      </div>
+    );
+  } catch (error) {
+    return (
+      <>
+        <h3>Error n ShowImage encountered</h3>
+        <div>{error.message}</div>
+      </>
+    );
+  }
 }
 export default ShowLocalImage;
