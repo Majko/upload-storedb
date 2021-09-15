d3fe0435f75c3c99cc178847578d73d8b2e30929
Majko <mrnhrnk@gmail.com>
2021-09-15 21:42:36 +0200

    visual

--- src/Components/composition/ItemFullDetail.js
+++ src/Components/composition/ItemFullDetail.js
@@ -1,18 +1,22 @@
-import { Grid } from "@material-ui/core";
+import { Card, Container, Grid } from "@material-ui/core";
 
 const ItemFullDetail = ({ item }) => {
   return (
-    <Grid container spacing={0}>
-      <Grid item xs={12}>
-        <h3>FULL detail for item</h3>
-      </Grid>
-      <Grid item xs={2}>
-        <p>{item.name}</p>
-      </Grid>
-      <Grid item xs={10}>
-        <p>{item.content}</p>
-      </Grid>
-    </Grid>
+    <Card raised={1}>
+      <Container>
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            <h3>FULL detail for item</h3>
+          </Grid>
+          <Grid item xs={2}>
+            <p>{item.name}</p>
+          </Grid>
+          <Grid item xs={10}>
+            <p>{item.content}</p>
+          </Grid>
+        </Grid>
+      </Container>
+    </Card>
   );
 };
 
--- src/Components/composition/ItemShortDetail.js
+++ src/Components/composition/ItemShortDetail.js
@@ -1,18 +1,18 @@
-import { Grid } from "@material-ui/core";
+import { Card, Grid } from "@material-ui/core";
 
 const ItemShortDetail = ({ item }) => {
   return (
-    <Grid container spacing={0}>
-      <Grid item xs={10}>
-        <h3>itemized detail for: </h3>
+      <Grid container spacing={0}>
+        <Grid item xs={10}>
+          <h3>itemized detail for: </h3>
+        </Grid>
+        <Grid item xs={2}>
+          <h3>{item.name}</h3>
+        </Grid>
+        <Grid item xs={12}>
+          <p>{item.content}</p>
+        </Grid>
       </Grid>
-      <Grid item xs={2}>
-        <h3>{item.name }</h3>
-      </Grid>
-      <Grid item xs={12} >
-        <p>{item.content}</p>
-      </Grid>
-    </Grid>
   );
 };
 
