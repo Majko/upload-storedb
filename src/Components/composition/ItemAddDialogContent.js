1a5426791533e083c33fab4593d8de9a65c2b5dc
Majko <mrnhrnk@gmail.com>
2021-09-19 22:54:01 +0200

    rozsirenie

--- src/Components/composition/ItemAddDialogContent.js
+++ src/Components/composition/ItemAddDialogContent.js
@@ -1,21 +1,87 @@
-import { Button } from "@material-ui/core";
-import { useContext } from "react";
+import { Box, Button, Grid, TextField } from "@material-ui/core";
+import { useContext, useState } from "react";
 import { AddDialogContext } from "./DocList"; //musime naimportovat Context na otvor/close Dialog
 
-const ItemAddDialogContent = () => {
-  const {addDialogOpen, setAddDialogOpen} = useContext(AddDialogContext) // vyziadaj funkciu na zatvorenie
+const ItemAddDialogContent = ({ addItem }) => {
+  const { addDialogOpen, setAddDialogOpen } = useContext(AddDialogContext); // vyziadaj funkciu na zatvorenie
+  const [formstate, setFormstate] = useState({});
 
-const handleSave = ()=>{
-  //save item
-  setAddDialogOpen(false)
-}
+  const handleSave = (e) => {
+    addItem(formstate);
+    setAddDialogOpen(false);
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setFormstate({ ...formstate, [e.target.name]: value });
+  };
 
   return (
     <>
       <h3>Pridavam nieco</h3>
-      <Button variant="contained" color="primary" onClick={handleSave}>Uloz</Button>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              id="id"
+              label="Id"
+              variant="standard"
+              name="id"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              id="name"
+              label="Name"
+              variant="standard"
+              name="name"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              id="content"
+              label="Content"
+              variant="standard"
+              name="content"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              id="status"
+              label="Status"
+              variant="standard"
+              name="status"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              id="date"
+              label="Date"
+              variant="standard"
+              name="date"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="primary" onClick={handleSave}>
+              Uloz
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 };
 
-export default ItemAddDialogContent
+export default ItemAddDialogContent;
--- src/Components/composition/ItemFullDetail.js
+++ src/Components/composition/ItemFullDetail.js
@@ -1,24 +1,41 @@
-import { Button, Card, Container, Grid } from "@material-ui/core";
-import { DetailDialogContext } from "./DocListItem";
+import { Button, Container, Grid } from "@material-ui/core";
+import { DetailDialogContext } from "./DocList";
 import { useContext } from "react";
 
-const ItemFullDetail = ({ item }) => {
-  const { detailDialogOpen, setDetailDialogOpen } = useContext(
+const ItemFullDetail = ({ item , deleteItem}) => {
+  const { setDetailDialogOpen, detailDialogItem } = useContext(
     DetailDialogContext
   );
+
+  const handleDelete = () => {
+    deleteItem(detailDialogItem.id)
+    setDetailDialogOpen(false)
+  };
 
   return (
     <div>
       <Container>
         <Grid container spacing={0}>
           <Grid item xs={12}>
+            <Button onClick={handleDelete}>Zmaz</Button>
+          </Grid>
+          <Grid item xs={12}>
             <h3>FULL detail for item</h3>
           </Grid>
           <Grid item xs={2}>
-            <p>{item.name}</p>
+            <p>{detailDialogItem.id}</p>
+          </Grid>
+          <Grid item xs={2}>
+            <p>{detailDialogItem.name}</p>
           </Grid>
           <Grid item xs={10}>
-            <p>{item.content}</p>
+            <p>{detailDialogItem.content}</p>
+          </Grid>
+          <Grid item xs={2}>
+            <p>{detailDialogItem.date}</p>
+          </Grid>
+          <Grid item xs={2}>
+            <p>{detailDialogItem.status}</p>
           </Grid>
           <Grid item xs={12}>
             <Button
