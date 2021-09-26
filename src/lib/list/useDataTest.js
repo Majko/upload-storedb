a8b6dfa5cfa1240f68841720c5f691ebdf8a7bf1
Majko <mrnhrnk@gmail.com>
2021-09-26 11:39:48 +0200

    hook na data nacitanie

---
+++ src/lib/list/useDataTest.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+
+import { API } from "aws-amplify";
+import * as queries from "../../graphql/queries";
+import * as mutations from "../../graphql/mutations";
+
+/**
+ *
+ * @param {Integer} initialPageItems number of page items that will be loaded at the begining
+ * @returns {Object}  { dataArray, nextPage, addItem, modifyItem, deleteItem }
+ */
+const useDataTest = (initialPageItems) => {
+  const [dataArray, setDataArray] = useState([]);
+  const [nextToken, setNextToken] = useState(null);
+
+  useEffect(() => {
+    nextPage(initialPageItems);
+  }, []);
+
+  const nextPage = async (pageItems) => {
+    //   in case of previous nextToken was null => no new item to paginate, we end
+    if (nextToken === "END") return;
+    const list = await API.graphql({
+      query: queries.listClanoks,
+      variables: {
+        limit: pageItems,
+        nextToken: nextToken,
+      },
+    });
+    const items = list.data.listClanoks.items;
+    const newNextToken = list.data.listClanoks.nextToken;
+    // if no new items available, set the nexToken to 'END'
+    newNextToken === null ? setNextToken("END") : setNextToken(newNextToken);
+    setDataArray((prevList) => [...prevList, ...items]);
+  };
+
+  const addItem = async (item) => {
+    const newItem = await API.graphql({
+      query: mutations.createClanok,
+      variables: {
+        input: {
+          name: item.name,
+          content: item.content,
+          status: item.status,
+        },
+      },
+    });
+    setDataArray((previous) => [...previous, newItem.data.createClanok]);
+  };
+
+  const modifyItem = async (id, item) => {
+    const modifiedItem = await API.graphql({
+      query: mutations.createClanok,
+      variables: {
+        input: {
+          id: id,
+          name: item.name,
+          content: item.content,
+          status: item.status,
+        },
+      },
+    });
+    console.log(modifiedItem);
+    const newArray = dataArray.map((elem) => {
+      return elem.id === id ? item : elem;
+    });
+    setDataArray(newArray);
+  };
+
+  const deleteItem = async (id) => {
+    await API.graphql({
+      query: mutations.deleteClanok,
+      variables: {
+        input: {
+          id: id,
+        },
+      },
+    });
+
+    let newArray = [];
+    dataArray.forEach((elem) => {
+      if (elem.id !== id) newArray.push(elem);
+    });
+    setDataArray(newArray);
+  };
+
+  return { dataArray, nextPage, addItem, modifyItem, deleteItem };
+};
+
+export default useDataTest;
