37981e25861738829c2d2cc05f7075c6a7b06b9c
Majko <mrnhrnk@gmail.com>
2021-11-18 15:08:53 +0100

    Upgrade Amplify cli

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "uploadstoredb5ce5bad7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "uploadstoredb": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "testUploadStor": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
