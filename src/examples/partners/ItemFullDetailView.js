945bb0f51fec05a3be7d875e39ad9a888fe04e6a
Majko <mrnhrnk@gmail.com>
2021-11-21 09:05:04 +0100

    Upratanie nepotrebnych Grid

--- src/examples/partners/ItemFullDetailView.js
+++ src/examples/partners/ItemFullDetailView.js
@@ -1,75 +1,63 @@
-import { Button, Container, Grid, Typography } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
 
-const ItemFullDetailView= ({ item, deleteItem }) => {
+const ItemFullDetailView = ({ item, deleteItem }) => {
   return (
     <div>
-        <Grid container spacing={0}>
-          <Grid item xs={12} md={12}>
-            <Typography color="textPrimary" component={'span'} variant="h5">
-              {item.company}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={12}>
-            <Typography color="textSecondary" gutterBottom component={'span'} variant="h6">
-              Mesto: {item.city}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography color="textSecondary" gutterBottom variant="caption">
-              Ulica: {item.street}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography color="textSecondary" gutterBottom variant="caption">
-              PSČ: {item.zip}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography color="textSecondary" gutterBottom variant="caption">
-              Meno: {item.name}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography color="textSecondary" gutterBottom variant="caption">
-              Divizia: {item.division}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography color="textSecondary" gutterBottom variant="caption">
-              IČO: {item.ico}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography color="textSecondary" gutterBottom variant="caption">
-              DIČ: {item.dic}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography color="textSecondary" gutterBottom variant="caption">
-              Email: {item.email}
-            </Typography>
-          </Grid>
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={12}>
+          <Typography color="textPrimary" component={"span"} variant="h5">
+            {item.company}
+          </Typography>
         </Grid>
-        <Grid container spacing={3}>
-          {/* <Grid item xs={3}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => setDetailDialogOpen(false)}
-            >
-              Zavri
-            </Button>
-          </Grid> */}
-          <Grid item xs={6}>
-            <Button
-              onClick={deleteItem}
-              variant="contained"
-              color="secondary"
-            >
-              Zmaž
-            </Button>
-          </Grid>
+        <Grid item xs={12} md={12}>
+          <Typography
+            color="textSecondary"
+            gutterBottom
+            component={"span"}
+            variant="h6"
+          >
+            Mesto: {item.city}
+          </Typography>
         </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography color="textSecondary" gutterBottom variant="caption">
+            Ulica: {item.street}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography color="textSecondary" gutterBottom variant="caption">
+            PSČ: {item.zip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography color="textSecondary" gutterBottom variant="caption">
+            Meno: {item.name}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography color="textSecondary" gutterBottom variant="caption">
+            Divizia: {item.division}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography color="textSecondary" gutterBottom variant="caption">
+            IČO: {item.ico}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography color="textSecondary" gutterBottom variant="caption">
+            DIČ: {item.dic}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography color="textSecondary" gutterBottom variant="caption">
+            Email: {item.email}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Button onClick={deleteItem} variant="contained" color="secondary">
+        Zmaž
+      </Button>
     </div>
   );
 };
