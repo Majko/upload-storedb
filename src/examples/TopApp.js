ee266293c46314953af9b4d3b0ad073b179f9e24
Majko <mrnhrnk@gmail.com>
2021-12-12 11:22:39 +0100

    Zmena Login screen

--- src/examples/TopApp.js
+++ src/examples/TopApp.js
@@ -18,22 +18,30 @@ import {
 import { routesConfig } from "./routesConfig";
 import MainApp from "../lib/menu/MainApp";
 import { makeStyles } from "@mui/styles";
+import { blue, green, purple, red } from "@mui/material/colors";
 
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
 
 const theme = createTheme();
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  amplifyAuthenticator: {
+    background: blue[900],
+    padding: "25px",
+    amplifyPrimaryColor: green[500],
+  },
   login: {
-    amplifyPrimaryColor: "#4b1c08",
-    amplifyPrimaryTint: "#b36627",
+    amplifyPrimaryColor: green[500],
+    amplifyPrimaryTint: red[600],
     amplifyPrimaryShade: "#783b17",
     amplifyFontFamily: "Roboto",
     amplifyWhite: "#fdfade",
-    amplifySecondaryTint: "#b36627",
+    amplifySecondaryTint: red[600],
+    background: green[800],
+    color: red[300],
   },
-});
+}));
 
 function TopApp() {
   // vyzaduje a konfiguracia routes v tvare:
@@ -48,13 +56,13 @@ function TopApp() {
   const classes = useStyles();
 
   return (
-    <div className={classes.login}>
+    <div>
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={theme}>
-          <AmplifyAuthenticator >
+          <AmplifyAuthenticator>
             {
               <AmplifySignIn
-                
+                className={classes.amplifyAuthenticator}
                 slot="sign-in"
                 headerText="VITAJTE V UCTO.ONLINE!"
                 hideSignUp
@@ -69,7 +77,7 @@ function TopApp() {
                   {
                     type: "password",
                     label: "Heslo",
-                    placeholder: "keep it secret",
+                    placeholder: "Tajne heslo",
                     required: true,
                   },
                 ]}
